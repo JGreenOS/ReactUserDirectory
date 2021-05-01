e785bf474fd047f9929366a55417635e9069b505
JGreenOS <jsciteach@gmail.com>
2021-05-01 17:24:28 -0400

    added button, headers, navbar components

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+
+
+function NavBar () {
+    return (
+        <div className="navbar navbar-light bg-light fixed-top">
+        <nav class = "navbar navbar-light bg-light">
+        <span class="navbar-brand mb-0 h1">User Directory</span>   
+        </nav>
+        </div>
+    );
+}
+
+export default NavBar
---
+++ src/components/Users.js
@@ -0,0 +1,28 @@
+function users (props)  {
+    return (
+        <div>
+{/*Table starts here*/}
+<table className="table table-bordered table-sm">
+    <thead>
+        <tr>
+            <th className="col-3"><button onClick={props.filter}>Users (Filter by TBD)</button></th>
+        </tr>
+    </thead>
+</table>
+
+
+        </div>
+
+    )
+    
+}
+
+const Users = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Users
